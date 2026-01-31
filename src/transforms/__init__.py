f183f054e7cc93a1a71fe395442fd9e0e4015f09
JoustiEe <ro.kaanik@gmail.com>
2026-01-31 03:22:16 +0300

    cuout & sample plot added

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -5,3 +5,4 @@ from src.transforms.flip import RandFlip3D
 from src.transforms.shift_intensity import RandShiftIntensity3D
 from src.transforms.crop import RandSpatialCrop3D
 from src.transforms.post_process import PostProcess
+from src.transforms.cutout import Cutout3D
