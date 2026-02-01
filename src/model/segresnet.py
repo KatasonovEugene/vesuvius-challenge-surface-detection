ae5912ab4f0937d258fdbc7ac8d19dd00da2d6b4
Катасонов Евгений Алексеевич <eakatasonov_1@edu.hse.ru>
2026-02-01 17:43:12 +0300

    out_channels 3->2

--- src/model/segresnet.py
+++ src/model/segresnet.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 from monai.networks.nets import SegResNet
 
@@ -25,25 +24,3 @@ class SegResNetDetector(nn.Module):
         volume = volume.unsqueeze(1)
         logits = self.backbone(volume)    # (B, 2, D, H, W)
         return {'logits': logits}
-
-class SegResNetInferenceVersion(nn.Module):
-    def __init__(
-        self,
-        spatial_dims=3,
-        in_channels=1,
-        out_channels=2,
-        init_filters=16,
-        dropout_prob=0.2,
-    ):
-        super().__init__()
-        self.backbone = SegResNet(
-            spatial_dims=spatial_dims,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            init_filters=init_filters,
-            dropout_prob=dropout_prob,
-        )
-
-    def forward(self, volume):
-        logits = self.backbone(volume)
-        return logits
