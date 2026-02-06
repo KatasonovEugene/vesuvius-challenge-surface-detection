fbbe70a1d4d7e05517b94d3a0485dece9a1f85e6
JoustiEe <ro.kaanik@gmail.com>
2026-02-06 21:16:16 +0300

    compile_wrapper change

--- src/model/compile_wrapper.py
+++ src/model/compile_wrapper.py
@@ -3,15 +3,18 @@ import torch.nn as nn
 
 
 class CompileWrapper(nn.Module):
-    def __init__(self, model, **compile_kwargs):
+    def __init__(self, model, input_keys=None, **compile_kwargs):
         super().__init__()
         self._orig_mod = model
+        self.input_keys = input_keys
         self.compiled_model = torch.compile(model, **compile_kwargs)
 
     def get_inner_model(self):
         return self._orig_mod
 
     def forward(self, **batch):
+        if self.input_keys is not None:
+            batch = {key: batch[key] for key in self.input_keys if key in batch}
         return self.compiled_model(**batch)
 
     def state_dict(self, *args, **kwargs):
