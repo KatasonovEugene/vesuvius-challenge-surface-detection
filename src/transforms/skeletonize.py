a0bcb3e3de05b7250eb6b86bfe2f5efc82b72b6d
Катасонов Евгений Алексеевич <eakatasonov_1@edu.hse.ru>
2026-02-07 16:42:28 +0300

    tubed medial surface

--- src/transforms/skeletonize.py
+++ src/transforms/skeletonize.py
@@ -5,8 +5,9 @@ from scipy.ndimage import binary_dilation
 
 
 class Skeletonize(nn.Module):
-    def __init__(self):
+    def __init__(self, dilation_iterations=1):
         super().__init__()
+        self.dilation_iterations = dilation_iterations
 
     def forward(self, gt_skel=None, **batch):
         if gt_skel is None:
@@ -14,13 +15,13 @@ class Skeletonize(nn.Module):
         gt_skel = gt_skel.squeeze(axis=0)
         mask = (gt_skel == 1)
         skel = skeletonize(mask)
-        tubed_skel = binary_dilation(skel, iterations=1)
+        tubed_skel = binary_dilation(skel, iterations=self.dilation_iterations)
         tubed_skel = tubed_skel[None]
         return {"gt_skel": tubed_skel}
 
 
 class MedialSurface(nn.Module):
-    def __init__(self, pseudo3d=False, do_tube=False):
+    def __init__(self, pseudo3d=False, do_tube=False, dilation_iterations=1):
         """
         Calculates the medial surface skeleton of the segmentation (plus an optional 2 px tube around it) 
         and adds it to the dict with the key "skel"
@@ -28,6 +29,7 @@ class MedialSurface(nn.Module):
         super().__init__()
         self.pseudo3d = pseudo3d
         self.do_tube = do_tube
+        self.dilation_iterations = dilation_iterations
 
     def forward(self, gt_skel=None, **batch):
         if gt_skel is None:
@@ -51,7 +53,8 @@ class MedialSurface(nn.Module):
                     skel[:, :, x] |= skeletonize(mask[:, :, x])
 
             if self.do_tube:
-                skel = dilation(dilation(skel))
+                skel = binary_dilation(skel, iterations=self.dilation_iterations)
+
             skel &= gt_skel.astype(bool)
             skel_all = skel
 
