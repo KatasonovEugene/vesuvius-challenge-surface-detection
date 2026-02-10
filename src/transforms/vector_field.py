e8796b77a09d4545285aa89c544b1d2ca5825bf5
Катасонов Евгений Алексеевич <eakatasonov_1@edu.hse.ru>
2026-02-10 19:30:41 +0300

    gauss filter sigma->0.5 ; normals = -normals

--- src/transforms/vector_field.py
+++ src/transforms/vector_field.py
@@ -4,7 +4,8 @@ from torch import nn
 
 
 class VectorField3D(nn.Module):
-    def __init__(self):
+    def __init__(self, sigma=1.0):
+        self.sigma = sigma
         super().__init__()
 
     def forward(self, volume, gt_mask, gt_skel=None, **batch):
@@ -12,11 +13,11 @@ class VectorField3D(nn.Module):
             raise RuntimeError(f'VectorField3D: input shape was not expected; input shape: {volume.shape}; expected shape: [B, D, H, W]')
 
         dist = distance_transform_edt(1 - gt_mask[0].astype(np.float32))
-        dist_smooth = gaussian_filter(dist, sigma=1.0)
+        dist_smooth = gaussian_filter(dist, sigma=self.sigma)
         
         gz, gy, gx = np.gradient(dist_smooth)
         
-        normals = np.stack([gz, gy, gx], axis=0)
+        normals = -np.stack([gz, gy, gx], axis=0)
         
         norm = np.linalg.norm(normals, axis=0)
         normals = np.divide(normals, norm, out=np.zeros_like(normals), where=norm > 1e-6)
