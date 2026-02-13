34d6a4e1b6c80b15a5c5721bbd14229536c32c98
Катасонов Евгений Алексеевич <eakatasonov_1@edu.hse.ru>
2026-02-13 13:42:01 +0300

    temporary changes for baseline

--- src/transforms/sliding_window_artefacts.py
+++ src/transforms/sliding_window_artefacts.py
@@ -30,39 +30,39 @@ class RandPatchProbReduction(nn.Module):
         if teacher_probs.ndim != 4:
             raise RuntimeError(f'RandPatchProbReduce: input shape was not expected; input shape: {teacher_probs.shape}; expected shape: [B, D, H, W]')
 
-        B, D, H, W = teacher_probs.shape
-        device = teacher_probs.device
+        # B, D, H, W = teacher_probs.shape
+        # device = teacher_probs.device
 
-        apply_transform = torch.bernoulli(
-            torch.full(size=(B,), fill_value=self.prob, device=device)
-        ).to(dtype=teacher_probs.dtype).view(-1, 1, 1, 1)
+        # apply_transform = torch.bernoulli(
+        #     torch.full(size=(B,), fill_value=self.prob, device=device)
+        # ).to(dtype=teacher_probs.dtype).view(-1, 1, 1, 1)
 
-        if apply_transform.sum() == 0:
-            return {'teacher_probs': teacher_probs}
+        # if apply_transform.sum() == 0:
+        #     return {'teacher_probs': teacher_probs}
 
-        koefs = torch.empty(size=(B,), dtype=teacher_probs.dtype, device=device).uniform_(*self.koef_range)
+        # koefs = torch.empty(size=(B,), dtype=teacher_probs.dtype, device=device).uniform_(*self.koef_range)
 
-        patch_sizes = torch.randint(self.patch_size_range[0], self.patch_size_range[1], size=(B,), device=device)
-        start_ds = torch.randint(0, D - patch_sizes, size=(B,), device=device)
-        start_hs = torch.randint(0, H - patch_sizes, size=(B,), device=device)
-        start_ws = torch.randint(0, W - patch_sizes, size=(B,), device=device)
-        end_ds = start_ds + patch_sizes
-        end_hs = start_hs + patch_sizes
-        end_ws = start_ws + patch_sizes
+        # patch_sizes = torch.randint(self.patch_size_range[0], self.patch_size_range[1], size=(B,), device=device)
+        # start_ds = torch.randint(0, D - patch_sizes, size=(B,), device=device)
+        # start_hs = torch.randint(0, H - patch_sizes, size=(B,), device=device)
+        # start_ws = torch.randint(0, W - patch_sizes, size=(B,), device=device)
+        # end_ds = start_ds + patch_sizes
+        # end_hs = start_hs + patch_sizes
+        # end_ws = start_ws + patch_sizes
 
-        idx_d = torch.arange(D, device=device).view(1, D, 1, 1)
-        idx_h = torch.arange(H, device=device).view(1, 1, H, 1)
-        idx_w = torch.arange(W, device=device).view(1, 1, 1, W)
+        # idx_d = torch.arange(D, device=device).view(1, D, 1, 1)
+        # idx_h = torch.arange(H, device=device).view(1, 1, H, 1)
+        # idx_w = torch.arange(W, device=device).view(1, 1, 1, W)
 
-        mask_d = (idx_d >= start_ds.view(-1, 1, 1, 1)) & (idx_d < end_ds.view(-1, 1, 1, 1))
-        mask_h = (idx_h >= start_hs.view(-1, 1, 1, 1)) & (idx_h < end_hs.view(-1, 1, 1, 1))
-        mask_w = (idx_w >= start_ws.view(-1, 1, 1, 1)) & (idx_w < end_ws.view(-1, 1, 1, 1))
+        # mask_d = (idx_d >= start_ds.view(-1, 1, 1, 1)) & (idx_d < end_ds.view(-1, 1, 1, 1))
+        # mask_h = (idx_h >= start_hs.view(-1, 1, 1, 1)) & (idx_h < end_hs.view(-1, 1, 1, 1))
+        # mask_w = (idx_w >= start_ws.view(-1, 1, 1, 1)) & (idx_w < end_ws.view(-1, 1, 1, 1))
 
-        mask = (mask_d & mask_h & mask_w).to(dtype=teacher_probs.dtype)
+        # mask = (mask_d & mask_h & mask_w).to(dtype=teacher_probs.dtype)
 
-        sigmas = torch.empty(size=(teacher_probs.shape[0],), dtype=teacher_probs.dtype, device=device).uniform_(*self.sigma_range)
-        mask = gaussian_blur_batch_3d(mask, sigmas=sigmas)
+        # sigmas = torch.empty(size=(teacher_probs.shape[0],), dtype=teacher_probs.dtype, device=device).uniform_(*self.sigma_range)
+        # mask = gaussian_blur_batch_3d(mask, sigmas=sigmas)
 
-        teacher_probs = torch.clamp(teacher_probs * (1 - apply_transform * mask * (1 - koefs.view(-1, 1, 1, 1))), 0.0, 1.0)
+        # teacher_probs = torch.clamp(teacher_probs * (1 - apply_transform * mask * (1 - koefs.view(-1, 1, 1, 1))), 0.0, 1.0)
 
         return {'teacher_probs': teacher_probs}
